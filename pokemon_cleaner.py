720f28ab3d0546bb6f249ff5f372e5f5f314ba2e
Farzad Khalafi <farzadk@gmail.com>
2023-06-03 22:37:00 -0400

    finished data reading process

--- pokemon_cleaner.py
+++ pokemon_cleaner.py
@@ -1,20 +1,39 @@
 from screen_controller import ScreenController
 #from pokemon_database import PokemonDatabase
+import json
+import csv
+import time
 
 
-def main():
-    screen_reader = ScreenController()
+def getPokemon(screen_reader):
     pokemon = {}
     pokemon['name'] = screen_reader.readName()
     pokemon['cp'] = screen_reader.readCP()
     pokemon['hp'] = screen_reader.readHP()
-    pokemon['ivs'] = screen_reader.readIVs()
-
+    (pokemon['attack'],pokemon['defense'],pokemon['stamina']) = screen_reader.readIVs()
 
     #pokemon_details = PokemonDatabase.findPokemon(name:pokemon.name)
     #pokemon.family = pokemon_details.family
 
-    print(pokemon)
+    return pokemon
+
+
+def main():
+    screen_reader = ScreenController()
+    for i in range(1900):
+        #current_time = time.time()
+        file = open('pokemons.csv', 'a', newline='')
+        writer = csv.writer(file)
+        #print("Time passed:", time.time() - current_time, "seconds")
+        screen_reader.takeScreenshot()
+        pokemon = getPokemon(screen_reader)
+        print(pokemon)
+        #name,cp,hp,attack,defense,stamina
+        writer.writerow((pokemon['name'],pokemon['cp'],pokemon['hp'],pokemon['attack'],pokemon['defense'],pokemon['stamina']))
+        screen_reader.gotoNext()
+
+        file.close()
+
 
 
 main()
